73752a9d27426e691340c1a84a68b2254fe50318
magacademy8 <magacademy313@gmail.com>
2025-06-29 01:13:50 +0300

    Rename _services_hizmet1_Version2.md to _services_hizmet1_Version2.tsx

--- _services_hizmet1_Version2.md
+++
@@ -1,8 +0,0 @@
----
-layout: service
-title: "Usui Reiki'yle Şifa"
-lang: tr
-price: 1000₺
----
-
-Usui Reiki'yle Şifa hizmetini hayatının her alanı için satın alabilirsin.
---
+++ _services_hizmet1_Version2.tsx
@@ -0,0 +1,8 @@
+---
+layout: service
+title: "Usui Reiki'yle Şifa"
+lang: tr
+price: 1000₺
+---
+
+Usui Reiki'yle Şifa hizmetini hayatının her alanı için satın alabilirsin.
